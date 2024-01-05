cac2f0ca31601a51dd3999a5a91a97a124b5f7d9
Jacob Palmer <palmer.904@buckeyemail.osu.edu>
2024-01-05 14:14:25 -0500

    version 0.0.5

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
+	before_create :set_default_values
+
+	private
+
 	has_secure_password
-	#has_many :scores, dependent: :destroy
 	validates :user_name, presence: true, uniqueness: true
 	validates_format_of :user_name, with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d_-]{5,20}\z/, message: "should contain at least one letter and one digit, and be between 5 and 20 characters in length"
+
+	def set_default_values
+    	self.game_count ||= 0
+    	self.sets_found ||= 0
+  	end
 end
